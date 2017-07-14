dde090291b180a39fe28ccd6a21ac0f1c36c23c4
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-07-13 17:13:51 -0700

    MENSAJE

---
+++ assets/js/javascript.js
@@ -0,0 +1,49 @@
+
+var productos = [];
+var records = document.getElementById('lista');
+
+function Producto(compra) {
+  this.compra = compra.toLowerCase();
+  this.producttID = (productos.length + 1);
+ 
+};
+
+Producto.prototype.toHTML = function () {
+  var html = '';
+  html += this.compra.toUpperCase() + '<br>';
+  html += '<br>';
+  return html;
+}
+
+function mergeHTML (){
+  var html = '';
+  for (i=0; i<productos.length; i++){   
+    html += productos[i].toHTML();  
+  }
+  return html;
+}
+
+function printHTML (html){
+  records.innerHTML = '';
+  records.innerHTML = html;  //variable mal escrita htm
+}
+
+var addCompra = document.getElementById('nuevacompra');
+
+
+addCompra.onclick = function() {
+  var compra = document.getElementById('frutas').value;
+  //var compra = prompt('Ingrese su nueva compra');
+  //var cantidad = prompt('Ingrese la cantidad');
+  var product  = new Producto (compra);
+  productos.push(product);
+  printHTML(product.toHTML());
+  printHTML(mergeHTML());
+};
+
+
+// al hacerle click a mostrar todas las compras, imprime todas las compras en el html. ESTA ESTÁ BUENA, NO CAMBIAR NADA
+/*var printAll = document.getElementById('print');
+printAll.onclick = function() {
+  printHTML(mergeHTML());
+}*/
